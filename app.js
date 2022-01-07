1fa8f7cfa7d0904950670f735bef82ce2ca5df1e
Sovd <navaansovd0527@gmail.com>
2022-01-07 19:55:56 +0800

    Shoog shiddeg bolgolloo

--- app.js
+++ app.js
@@ -6,12 +6,17 @@ var scores = [0, 0];
 var roundScore = 0;
 
 // shoo ali talaaraa buusaniig hadgalah huwisagch, 1-6 gesen huwisagch olgono
-var dice = Math.floor(Math.random() * 6) + 1;
 
 // document.querySelector("#score-0").textContent = dice;
 //program ehlehed beltgey
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
-document.querySelector(".dice").style.display = "none";
+var diceDom = document.querySelector(".dice");
+document.getElementById("score-0").textContent = 0;
+document.getElementById("score-1").textContent = 0;
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
+diceDom.style.display = "none";
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+});
